ab0716e4b02e2cb08baa8bc96190e012048ad900
Yosif Georgiev <iodesign@live.com>
2023-03-10 14:05:19 +0200

    UPDATE: Task-9 CSS  Thermostat component

--- src/components/thermostat/Thermostat.jsx
+++ src/components/thermostat/Thermostat.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import styles from "./Thermostat.module.scss";
 import classNames from "classnames";
+import { red } from "@mui/material/colors";
 
 import CircularProgress from "@mui/material/CircularProgress";
 import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
@@ -20,6 +21,8 @@ export default function Thermostat({
     { temperature: 10, hour: 17 },
   ],
 }) {
+  const color = red[800];
+
   const [currentTemp, setCurrentTemp] = useState(15);
 
   return (
@@ -31,11 +34,12 @@ export default function Thermostat({
           thickness={3}
           className={classNames(styles.progressOne)}
           style={{
+            color: red[700],
             position: "absolute",
             transform: "rotate(180deg)",
           }}
           value={50}
-        ></CircularProgress>
+        />
         <CircularProgress
           variant="determinate"
           size={433}
@@ -47,57 +51,65 @@ export default function Thermostat({
             transform: "rotate(180deg)",
           }}
           value={((currentTemp - 15) * 50) / 35}
-        ></CircularProgress>
-        <div 
-        className={classNames(styles.tempBtns)}
-          style={{
-           
-          }}
-        >
+        />
+        <div className={classNames(styles.tempBtns)} style={{}}>
           <Stack direction="row" justifyContent="center" alignItems="center">
-            <IconButton
-              color="inherit"
-              onClick={() =>
-                setCurrentTemp(
-                  currentTemp == 15 ? currentTemp : currentTemp - 1
-                )
-              }
-            >
-              <RemoveCircleOutlinedIcon fontSize="large"></RemoveCircleOutlinedIcon>
-            </IconButton>
-            <Typography variant="h2">{currentTemp}°</Typography>
-            <IconButton
-              color="inherit"
-              onClick={() =>
-                setCurrentTemp(
-                  currentTemp == 50 ? currentTemp : currentTemp + 1
-                )
-              }
-            >
-              <AddCircleOutlinedIcon fontSize="large"></AddCircleOutlinedIcon>
-            </IconButton>
+            <div className={classNames(styles.thermostat)}>
+              <IconButton
+                className={classNames(styles.therBtn)}
+                onClick={() =>
+                  setCurrentTemp(
+                    currentTemp == 15 ? currentTemp : currentTemp - 1
+                  )
+                }
+              >
+                <RemoveCircleOutlinedIcon fontSize="large" />
+              </IconButton>
+              <Typography
+                variant="h1"
+                className={classNames(styles.currentTemp)}
+              >
+                {currentTemp}°
+              </Typography>
+              <IconButton
+                className={classNames(styles.therBtn)}
+                onClick={() =>
+                  setCurrentTemp(
+                    currentTemp == 50 ? currentTemp : currentTemp + 1
+                  )
+                }
+              >
+                <AddCircleOutlinedIcon fontSize="large" />
+              </IconButton>
+            </div>
           </Stack>
         </div>
       </div>
-      <Typography
-        variant="h6"
-        style={{
-          color: "white",
-          position: "absolute",
-          marginTop: "150px",
-          marginLeft: "5px",
-        }}
-      >
-        Temperature Graph
-      </Typography>
-      <ComposedChart
-        width={595}
-        height={190}
-        data={data}
-        style={{ position: "absolute", marginTop: "160px", marginLeft: "5px" }}
-      >
-        <Line stroke="#ffffff" type="monotone" dataKey="temperature" />
-      </ComposedChart>
+      <div className={classNames(styles.chart)}>
+        <Typography
+          variant="h"
+          style={{
+            color: "white",
+            position: "absolute",
+            marginTop: "150px",
+            marginLeft: "5px",
+          }}
+        >
+          Temperature Graph
+        </Typography>
+        <ComposedChart
+          width={595}
+          height={190}
+          data={data}
+          style={{
+            position: "absolute",
+            marginTop: "160px",
+            marginLeft: "5px",
+          }}
+        >
+          <Line stroke="#ffffff" type="monotone" dataKey="temperature" />
+        </ComposedChart>
+      </div>
     </div>
   );
 }
