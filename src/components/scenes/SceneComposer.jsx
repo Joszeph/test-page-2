63601641e0cacdd99ffa0f63c867081a104f0ce9
Yosif Georgiev <iodesign@live.com>
2023-03-16 18:03:34 +0200

    NEW: SceneComposer component

--- src/components/scenes/SceneComposer.jsx
+++ src/components/scenes/SceneComposer.jsx
@@ -1,57 +1,50 @@
-import styles from "./SceneComposer.module.scss";
-import classNames from "classnames";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography } from '@mui/material';
+import styles from './SceneComposer.module.scss';
+import Scenes from './Scenes';
+import { useMemo, useState } from 'react';
 
-import Scenes from "./Scenes";
-import { useMemo, useState } from "react";
+export default function SceneComposer({ devices = [], rooms = [], selected, onScene }) {
+  const [status, setStatus] = useState('off');
 
-export default function SceneComposer({
-  devices = [],
-  rooms = [],
-  selected,
-  onScene,
-}) {
-  const [status, setStatus] = useState("off");
-
-  const group = useMemo(() => {
+  const groupBy = useMemo(() => {
     let id = 1;
-    const groupedDevices = rooms.map((room) => {
-      const sortedCards = devices.filter(
-        (device) => device?.roomId === room?.id
-      );
+    const groupedDevices = rooms.map(room => {
+      const sortedCards = devices.filter(device => device?.roomId === room?.id);
       const cards = [];
-      sortedCards.forEach((device) => {
+      sortedCards.forEach(device => {
         cards.push(
           {
             ...device,
-            title: "ON",
-            id: id++,
-            status: "on",
+            "title": "ON",
+            "id": id++,
+            "status": "on"
           },
           {
             ...device,
-            title: "OFF",
-            id: id++,
-            status: "off",
-          }
-        );
-      });
+            "title": "OFF",
+            "id": id++,
+            "status": "off"
+          },
+        )
+      })
 
+      console.log(cards);
       return {
-        id: room.id,
-        name: room.name,
-        cards: cards,
-      };
+        "id": room.id,
+        "name": room.name,
+        "cards": cards,
+      }
     });
 
-    return groupedDevices;
+    return groupedDevices
   }, [devices, rooms]);
 
+
   return (
-    <div className={classNames(styles.wrapper)}>
+    <div className={styles.wrapper}>
       <Grid container direction="column" spacing={4} className={styles.container}>
-        {group.map((item, index) => {
-          if (group[index].cards.length) {
+        {groupBy.map((item, index) => {
+          if (groupBy[index].cards.length) {
             return (
               <Grid item key={index}>
                 <Typography variant="h4">{item.name}</Typography>
